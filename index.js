7416dfead57bae58f5c57888603fbe0e839a762e
Yoni Slotwiner <yonislots@gmail.com>
2017-10-01 22:08:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 };
 
 function deleteFromObjectByKey(object,key){
-  Object.assign(delete object[key]);
+  Object.assign(object, delete object[key]);
   return object 
 }
